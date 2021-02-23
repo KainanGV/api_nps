233552f3ff5e9af5bf4f1e02660f3fb24bb97365
Kainan Gabriel <kainangabriel2019@gmail.com>
2021-02-22 22:39:29 -0300

    Atualizado configs do ts e rotas

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (req,res) => {
+    
+})
+
+const PORT = '8081'
+app.listen(PORT, () => {
+    console.log("SERVER ROLANDO")
+})
+
